c0ab79748c2afc40321ada211b580939ebebfdef
Thomas Ellison <thomasjlsn@gmail.com>
2020-03-24 14:44:13 -0700

    fix all pylint errors

--- slugger.py
+++ slugger.py
@@ -6,36 +6,36 @@ import re
 from sys import argv
 
 
-pullwords = []
+PULLWORDS = []
 
 with open('pullwords.txt', 'r') as pw:
-    # populate pullwords list
+    # Populate PULLWORDS list.
     for line in pw.readlines():
-        pullwords.append(line.strip())
+        PULLWORDS.append(line.strip())
 
 
 def swap_chars(title):
-    # swap [N] chars for dashes
+    """Swap [N] chars for dashes."""
     return re.sub('[~—|]', '-', title, flags=re.MULTILINE)
 
 
 def scrub_chars(title):
-    # keep only [^N] chars
+    """Keep only [^N] chars."""
     return re.sub('[^a-zA-Z0-9 ~-]', '', title, flags=re.MULTILINE)
 
 
 def hyphenate(title):
-    # reduce [N] chars to a dash
+    """Reduce [N] chars to a dash."""
     return re.sub('[ -]+', '-', title, flags=re.MULTILINE)
 
 
 def filter_pullwords(title_list):
-    # remove words from pullwords list
-    return [x for x in title_list if x not in pullwords]
+    """Remove words from PULLWORDS list."""
+    return [x for x in title_list if x not in PULLWORDS]
 
 
 def slugger(title_raw):
-    # convert string of words to URL slug
+    """Convert string of words to URL slug."""
     return hyphenate(swap_chars(scrub_chars(
         ' '.join(
             filter_pullwords(
@@ -43,6 +43,7 @@ def slugger(title_raw):
 
 
 def copy_to_clipboard(slug):
+    """Copy output of slugger() to system clipboard."""
     if name == 'posix':  # Linux
         system(f'echo "{slug}" | xsel --clipboard')
     elif name == 'nt':  # Windows
@@ -53,18 +54,18 @@ def copy_to_clipboard(slug):
 if __name__ == '__main__':
     try:
         while True:
-            user_input = input('Enter title: ')
+            USER_INPUT = input('Enter title: ')
 
-            if user_input == 'quit' or user_input == 'exit':
+            if USER_INPUT in ('exit', 'q', 'quit'):
                 exit(0)
 
             try:
                 if argv[1] == '-r' or argv[1] == '--raw':
-                    print(slugger(user_input))
+                    print(slugger(USER_INPUT))
                     exit(0)
             except IndexError:
                 pass  # in lieu of proper arg handling...
 
-            copy_to_clipboard(slugger(user_input))
+            copy_to_clipboard(slugger(USER_INPUT))
     except KeyboardInterrupt:
         exit(1)
