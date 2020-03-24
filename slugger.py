5bae402a39fee7cecff40b3804d916678008e659
Thomas Ellison <thomasjlsn@gmail.com>
2020-03-24 13:49:12 -0700

    added main loop. handle multiple dashes

--- slugger.py
+++ slugger.py
@@ -2,7 +2,7 @@
 """slugger.py"""
 
 from os import name, system
-from re import sub
+import re
 from sys import argv
 
 
@@ -16,17 +16,17 @@ with open('pullwords.txt', 'r') as pw:
 
 def swap_chars(title):
     # swap [N] chars for dashes
-    return sub('[~—|]', '-', title)
+    return re.sub('[~—|]', '-', title)
 
 
 def scrub_chars(title):
     # keep only [^N] chars
-    return sub('[^a-zA-Z0-9 ~]', '', title)
+    return re.sub('[^a-zA-Z0-9 ~]', '', title)
 
 
 def hyphenate(title):
     # reduce [N] chars to a dash
-    return sub('[ -]+', '-', title)
+    return re.sub('[ -]+', '-', title, flags=re.MULTILINE)
 
 
 def filter_pullwords(title_list):
@@ -36,7 +36,7 @@ def filter_pullwords(title_list):
 
 def slugger(title_raw):
     # convert string of words to URL slug
-    return swap_chars(hyphenate(scrub_chars(
+    return hyphenate(swap_chars(scrub_chars(
         ' '.join(
             filter_pullwords(
                 title_raw.lower().split()))))).strip(' -')
@@ -44,19 +44,24 @@ def slugger(title_raw):
 
 def copy_to_clipboard(slug):
     if name == 'posix':  # Linux
-        system(f'echo {slug} | xsel --clipboard')
+        system(f'echo "{slug}" | xsel --clipboard')
     elif name == 'nt':  # Windows
-        system(f'echo {slug} | clip')
+        system(f'echo {slug}| clip')
     print(f'copied "{slug}" to clipboard')
 
 
 if __name__ == '__main__':
-    user_input = input('Enter title: ')
-    try:
-        if argv[1] == '-r' or argv[1] == '--raw':
-            print(slugger(user_input))
-            exit(0)
-    except IndexError:
-        pass  # in lieu of proper arg handling...
+    while True:
+        user_input = input('Enter title: ')
 
-    copy_to_clipboard(slugger(user_input))
+        if user_input == 'quit' or user_input == 'exit':
+            exit(0)
+
+        try:
+            if argv[1] == '-r' or argv[1] == '--raw':
+                print(slugger(user_input))
+                exit(0)
+        except IndexError:
+            pass  # in lieu of proper arg handling...
+
+        copy_to_clipboard(slugger(user_input))
